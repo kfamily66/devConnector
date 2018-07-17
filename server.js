4a12489c02099a1f2af0a722da311428efa5b9ed
unknown <kfamily66@yandex.ru>
2018-07-17 19:51:23 +0500

    Add authentication using passport and jwt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -25,13 +26,15 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+// Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport.js")(passport);
+
 //Use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
-
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
