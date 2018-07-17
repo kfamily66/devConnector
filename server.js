064d528bed399afe9c716ac71f7044a293fadbc7
unknown <kfamily66@yandex.ru>
2018-07-17 17:21:49 +0500

    Configure remote mondoDB

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Hello");
